a728630b18c66dadbb2394e003049d9014936a08
rasika05 <rasikadeshpande511@gmail.com>
2017-09-26 17:38:29 +0530

    Modified file reference test

--- stock_exchange_application/src/main/java/com/atos/stock/services/Test.java
+++ stock_exchange_application/src/main/java/com/atos/stock/services/Test.java
@@ -7,6 +7,9 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.sql.Timestamp;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
 import java.util.Date;
 
 import com.atos.stock.dao.FileReferenceDAO;
@@ -14,50 +17,34 @@ import com.atos.stock.impl.FileReferenceDAOImpl;
 import com.atos.stock.model.FileReference;
 import com.atos.stock.utils.MyUtil;
 
-
-
 public class Test {
 
-	public static void main(String[] args) throws IOException {
-		
-		
+	public static void main(String[] args) throws IOException, ParseException {
 		
 		FileReference files = new FileReference();
-		files.setFileId(9989);
-		files.setFileName("doc");
 		
+		files.setFileName("samp");
 		
 		Date d=new Date();
-		Timestamp st=new Timestamp(d.getTime());
-		files.setFileProcessingData(st);
-		
-		
+		files.setFileProcessingData(d);
 		
 		Path path = Paths.get("D:/gayathri/proj_demo/s.txt");
 		byte[] data = Files.readAllBytes(path);
 		files.setFileData(data);
 		
+		
 		FileReferenceDAO frd = new FileReferenceDAOImpl();
+	
 		frd.addFileReference(files);
+		
 		frd.readFileReference(data);
-		frd.deleteFileReference(files.getFileId());
-	frd.deleteFileReferenceDate(st);
+		
+		files=frd.getFile(2030);
+		
+		//frd.deleteFileReference(files.getFileId());
+		
 		MyUtil.commitSession();
 		System.out.println("obj inserted");
-		for(byte b:data){
-			      	System.out.println((char)b);
-			        	
-			    }
-		
-	//	
-		
-
-		
-		
-	//	
-	//	
-	//	System.out.println("dleted");
-       
 		
 		
 	}
