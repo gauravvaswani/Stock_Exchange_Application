80ca309dfa3a824cbed117166a9b2b8312292800
dikshajasuja <30886884+dikshajasuja@users.noreply.github.com>
2017-09-26 17:40:00 +0530

    modified Watchlist Dao

--- stock_exchange_application/src/main/java/com/atos/stock/dao/WatchListDAO.java
+++ stock_exchange_application/src/main/java/com/atos/stock/dao/WatchListDAO.java
@@ -7,5 +7,7 @@ import model.WatchList;
 public interface WatchListDAO {
 	public void addWatchList(WatchList wl);
 	public void deleteWatchList(long watchlistId);
+	public void deleteWatchList(SeUser user);
+	public void deleteWatchList(StockData sd);
 	
 }
